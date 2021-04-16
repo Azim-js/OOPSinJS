7da8eac93676254f47e4c46836f90e3100f4e6e5
azim <az1234im@gmail.com>
2021-04-16 22:53:02 +0530

    use of objects (instance creation) and constructor

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,16 @@ class Product{
     title="Default";
     imgUrl;
     description;
+    price;
+
+    //adding a method a default constructor
+
+    constructor(title,imgUrl,price,description){
+        this.title=title;
+        this.imgUrl=imgUrl;
+        this.price=price;
+        this.description=description;
+    }
 }
 
 //object acsess by new keyword
@@ -11,27 +21,49 @@ console.log(new Product());
 
 const productList={
     products:[
-        // new Product();
-        {
-            title:'Pillow',
-            imgUrl:"https://secure.img1-fg.wfcdn.com/im/06073366/compr-r85/1145/114540090/interlude-luxurious-square-cotton-pillow-cover-and-insert.jpg",
-            price:699,
-            decription:"SOFT AND CUSHY"
-        },
-        {
-            title:'Chair',
-            imgUrl:"https://www.happybeds.co.uk/media/catalog/category/Sloane_Roomset_1.jpg",
-            price:5000,
-            decription:"ART LOOK"
-        },
+        // INSTANCE of CLASS use OF OOPS
 
-        // adding one more object
-        {
-            title:'Recliner',
-            imgUrl:"https://www.royaloakindia.com/uploads/ROYIND-royaloak-bently-recliner-1s-0125.webp",
-            price:4000,
-            description:'SOFT AND NICE'
-        }
+        new Product(
+            'Pillow',
+            "https://secure.img1-fg.wfcdn.com/im/06073366/compr-r85/1145/114540090/interlude-luxurious-square-cotton-pillow-cover-and-insert.jpg",
+            699,
+            "SOFT AND CUSHY"
+        ),
+        new Product(
+            'Chair',
+            "https://www.happybeds.co.uk/media/catalog/category/Sloane_Roomset_1.jpg",
+            5000,
+            "ART LOOK"
+        ),
+        new Product(
+            'Recliner',
+            "https://www.royaloakindia.com/uploads/ROYIND-royaloak-bently-recliner-1s-0125.webp",
+            4000,
+            'SOFT AND NICE'
+        ),
+
+
+
+        // {
+        //     title:'Pillow',
+        //     imgUrl:"https://secure.img1-fg.wfcdn.com/im/06073366/compr-r85/1145/114540090/interlude-luxurious-square-cotton-pillow-cover-and-insert.jpg",
+        //     price:699,
+        //     decription:"SOFT AND CUSHY"
+        // },
+        // {
+        //     title:'Chair',
+        //     imgUrl:"https://www.happybeds.co.uk/media/catalog/category/Sloane_Roomset_1.jpg",
+        //     price:5000,
+        //     decription:"ART LOOK"
+        // },
+
+        // // adding one more object
+        // {
+        //     title:'Recliner',
+        //     imgUrl:"https://www.royaloakindia.com/uploads/ROYIND-royaloak-bently-recliner-1s-0125.webp",
+        //     price:4000,
+        //     description:'SOFT AND NICE'
+        // }
     
     ],
     render(){
